4ba6df5a51e4a2bc20957b121c70acba3475d4b5
kjg <kimjg2477@gmail.com>
2026-06-27 14:07:13 +0900

    refactor: move nestia.config.ts to services/example-service

--- services/example-service/nestia.config.ts
+++ services/example-service/nestia.config.ts
@@ -1,10 +1,10 @@
 import { INestiaConfig } from "@nestia/sdk";
 
 const config: INestiaConfig = {
-  input: ["services/example-service/src/**/*.controller.ts"],
-  output: "services/example-service/dist/sdk",
+  input: ["src/**/*.controller.ts"],
+  output: "dist/sdk",
   swagger: {
-    output: "services/example-service/dist/swagger.json",
+    output: "dist/swagger.json",
     info: {
       title: "Example Service API",
       description: "Nestia-generated Swagger API docs for example-service",
